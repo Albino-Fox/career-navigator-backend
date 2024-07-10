37c42bd5c10d506d2c4bae4d7d9a1896aceed4cf
Albino-Fox <albinofox9@gmail.com>
2024-07-10 18:58:28 +1000

    rewrote model

--- src/models/test_subscribers.ts
+++ src/models/test_subscribers.ts
@@ -1,28 +1,20 @@
-import { Model, Table, Column, DataType } from "sequelize-typescript";
+import { Sequelize, DataTypes } from 'sequelize';
 
-@Table({
-  tableName: "test_subscribers",
-})
-class TestSubscribers extends Model {
-  @Column({
-    type: DataType.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    field: "id",
-  })
-  id?: number;
-
-  @Column({
-    type: DataType.STRING(255),
-    field: "nickname",
-  })
-  nickname?: string;
-
-  @Column({
-    type: DataType.DATE,
-    field: "subscription_time",
-  })
-  subscription_time?: string;
+module.exports = (sequelize: Sequelize) => {
+  return sequelize.define('TestSubscribers', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING(255),
+    },
+    subscription_date: {
+      type: DataTypes.DATE,
+    }
+  }, {
+    timestamps: false,
+    tableName: "test_subscribers",
+  });
 };
-
-module.exports = TestSubscribers;
