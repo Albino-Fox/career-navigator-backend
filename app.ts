90121c66e4024b2451aac4fd4e25c23703ce52d5
Albino-Fox <albinofox9@gmail.com>
2024-07-10 20:07:16 +1000

    creates table if nonexist and finally works without crash

--- app.ts
+++ app.ts
@@ -4,14 +4,25 @@ import {Request, Response} from "express"; // bruh
 const Database = require('@/database.ts')
 const config = require('@/config.ts');
 
-let app = express();
+async function main() {
+  let app = express();
+  app.listen(config.port, config.host, () => {
+    console.log(`Running on ${config.host} with port ${config.port}`);
+  })
 
-const db = new Database();
+  const db = new Database();
+  await db.sequelize.sync();
+  let records = await db.testSubscribers.findAll();
+  db.testSubscribers.create({
+    username: `testo${records.length}`,
+    subscription_date: new Date(Date.now()).toString(),
+  }).then((result) => console.log(`User ${result.username} created`))
+    .catch(err => console.error(err));
+  
+  app.get("/now", (req : Request, res : Response) => {
+    res.send(new Date(Date.now()).toString());
+  });
+  
+};
 
-app.get("/now", (req : Request, res : Response) => {
-  res.send(new Date(Date.now()).toString());
-});
-
-app.listen(config.port, config.host, () => {
-  console.log(`Running on ${config.host} with port ${config.port}`);
-})
+main();
--- src/database.ts
+++ src/database.ts
@@ -1,30 +1,36 @@
 const config = require('@/config.ts');
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize } from 'sequelize';
+const TestSubscribers = require('@/models/test_subscribers.ts');
 
 class Database {
   public sequelize: Sequelize | undefined;
-
+  public testSubscribers: Sequelize | undefined;
+  
   constructor() {
     this.connectToDatabase();
+    this.testSubscribers =  TestSubscribers( this.sequelize);
   }
+
   private async connectToDatabase() {
     this.sequelize = new Sequelize({
       host: config.db_host,
       username: config.db_user,
       password: config.db_password,
       database: config.db_name,
-      dialect: config.db_dialect,
+      dialect: config.db_dialect
     });
+
     
     await this.sequelize
-      .authenticate()
-      .then(() => {
-        console.log('Connection established.');
-      })
-      .catch((err) => {
-        console.error(`Connection failed: ${err}`);
-      });
+    .authenticate()
+    .then(() => {
+      console.log('Connection established.');
+    })
+    .catch((err) => {
+      console.error(`Connection failed: ${err}`);
+    });
   }
+
 }
 
 module.exports = Database;
